c532d6f2075dd5b564405e7373d26c698b957b97
nikitairs <stlrvnk@gmail.com>
2024-11-19 23:13:53 +0200

    play with stramlit

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World")
+import streamlit as st
+
+st.title("ai-scrapper")
+
+url_text_input = st.text_input("Enter the URL to scrap: ")
+
+if st.button("Start"):
+    print("Start...")
+    st.write(f"Scrapping {url_text_input}")
+
